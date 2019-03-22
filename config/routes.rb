ad27904ba532e3c2c7108557eae57501041357d6
yamato0331 <yamatotunasima1@gmail.com>
2019-03-22 10:03:14 +0900

    topページを表示しないように変更

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'screen/show'
 
   match "/*any", to: 'user#use', via: :all
+  get "/", to: 'user#use'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
